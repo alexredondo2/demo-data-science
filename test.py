a8fe516f967c445fb250039c3a1de2d2a9702c77
alexredondo2 <59750271+alexredondo2@users.noreply.github.com>
2021-11-12 16:05:18 +0100

    2 commit

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
 print ("Hello world!!")
+
+
+d= {}
+
+d["app - key"] = "apples - apples - hello world"
+d["bin - key"] = "bins - bins -hello word"
+d["dog - key"] = "dogs"
+d["ele - key"] = "elephants"
+
+def my_function():
+    pass
+
+def sarahs_function():
+    pass
+
+def mikes_function():
+    pass
